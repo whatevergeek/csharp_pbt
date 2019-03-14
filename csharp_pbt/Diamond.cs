c1ef9b6562068cbdce92e325fb18df2392289d5a
Paul Lorett Amazona <whatevergeek@gmail.com>
2019-03-14 18:11:48 +0800

    Refactored solution: Removed 'A' special case, Introduced mirrorAndFuse helper function, Renamed letters to indexedLetters

--- csharp_pbt/Diamond.cs
+++ csharp_pbt/Diamond.cs
@@ -7,31 +7,27 @@ namespace csharp_pbt
 {
     public static class Diamond
     {
+        static IEnumerable<T> MirrorAndFuse<T>(IEnumerable<T> l) => 
+            l.Concat(l.Reverse<T>().Skip<T>(1));
         static string MakeLine(int letterCount, char letter, int letterIndex)
         {
             var leadingSpace = new String(' ', letterCount - 1 - letterIndex);
             var innerSpace = new String(' ', letterCount - 1 - leadingSpace.Length);
-            switch (letter)
-            {
-                case 'A':
-                    return $"{leadingSpace ?? string.Empty}{letter}{leadingSpace ?? string.Empty}";
-                default:
-                    var left = 
-                        $"{leadingSpace ?? string.Empty}{letter}{innerSpace ?? string.Empty}"
-                        .ToList();
-                    return left.Concat(left.Reverse<char>().Skip(1))
-                        .Select(x => x.ToString())
-                        .Aggregate((x, y) => $"{x}{y}");
-            }
+            var left =
+                $"{leadingSpace ?? string.Empty}{letter}{innerSpace ?? string.Empty}"
+                .ToList();
+            return MirrorAndFuse(left)
+                .Select(x => x.ToString())
+                .Aggregate((x, y) => $"{x}{y}");
         }
 
         public static string Make(char letter)
         {
-            var letters = Enumerable.Range('A', letter - 'A' + 1).Select(i => (Char)i).ToArray()
+            var indexedLetters = Enumerable.Range('A', letter - 'A' + 1).Select(i => (Char)i).ToArray()
                 .Select((val, index) => (value: val, index: index));
 
-            return letters.Concat(letters.Reverse().Skip(1))
-                    .Select(s => MakeLine(letters.Count(), s.value, s.index))
+            return MirrorAndFuse(indexedLetters)
+                    .Select(s => MakeLine(indexedLetters.Count(), s.value, s.index))
                     .Aggregate((x, y) => $"{x}{Environment.NewLine}{y}");
         }
     }
